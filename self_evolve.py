6241b0dcc0871c3493e68acf2130a88c3cb4e655
DamjamRyuryu <140289472+DamjamRyuryu@users.noreply.github.com>
2024-10-22 03:39:31 +0800

    try no wrapping

--- self_evolve.py
+++ self_evolve.py
@@ -7,12 +7,11 @@ SYSTEM_PROMPT='''The user will ask you about Python code problem, follow their i
 # following prompts templates are based on the method from this paper:https://arxiv.org/pdf/2306.02907
 FIRST_STEP={
     'knowledge': "For the above question, could you briefly teach me how to solve it step by step in natural language?\nDon't write the code in this step.",
-    'solution': "Based on the above idea, help me complete the function.\nBe attention, you should only output the codes without any explanation, comment, natural language and testcode.\n\
-Warp your code with \"'''\""
+    'solution': "Based on the above idea, help me complete the function.\nBe attention, you should only output the codes without any explanation, comment, natural language and testcode."
 }
 SELF_REFINEMENT={
-    "syntax":"When I run this code, I meet %.\nHelp me refine the code.\nYou should only output the codes without any explanation, comment, natural language and testcode.\nWarp your code with \"'''\"",
-    "error":"I failed when going through the assertation:\n%\nHelp me refine the code.\nYou should only output the codes without any explanation, comment, natural language and testcode.\nWarp your code with \"'''\""
+    "syntax":"When I run this code, I meet %.\nHelp me refine the code.\nYou should only output the codes without any explanation, comment, natural language and testcode.",
+    "error":"I failed when going through the assertation:\n%\nHelp me refine the code.\nYou should only output the codes without any explanation, comment, natural language and testcode."
 }
 
 def get_input_list_se(evalset_file: str = HUMAN_EVAL):
